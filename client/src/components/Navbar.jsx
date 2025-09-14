c27752f855f9f8af7b757c510a634896fb565ab4
megasus777 <a.echechatoui7884@uca.ac.ma>
2025-09-14 10:17:54 +0100

    Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,11 +7,11 @@ export default function Navbar(){
         <nav className="w-full bg-black px-4 py-2 flex items-center justify-between border-b border-neutral-500 sticky top-0">
             <span className='text-orange-400 text-2xl font-bold'>Mega UDP</span>
             <div className='flex items-center gap-4 font-bold'>
-                <Link to={'/'} className='px-4 py-1 rounded-[4px] hover:bg-neutral-700 transition-all flex items-center gap-2 border-b-1 border-orange-700 hover:border-transparent'>
+                <Link to={'/'} className='bg-neutral-800 px-4 py-1 rounded-[4px] hover:bg-neutral-700 transition-all flex items-center gap-2 border-b-2 border-orange-700 hover:border-transparent'>
                     <AiOutlineHome size={18} />
                     <span>Home</span>
                 </Link>
-                <Link to={'/about'} className='px-4 py-1 rounded-[4px] hover:bg-neutral-700 transition-all flex items-center gap-2 border-b-1 border-orange-700 hover:border-transparent'>
+                <Link to={'/about'} className='bg-neutral-800 px-4 py-1 rounded-[4px] hover:bg-neutral-700 transition-all flex items-center gap-2 border-b-2 border-orange-700 hover:border-transparent'>
                     <AiOutlineInfoCircle size={18} />
                     <span>About</span>
                 </Link>
